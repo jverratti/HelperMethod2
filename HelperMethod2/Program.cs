b0f2183d8c5abae02d7fddf73401a0398e3fe024
jverratti <31357738+jverratti@users.noreply.github.com>
2017-09-18 18:55:18 -0600

    helperMethod-2-

---
+++ HelperMethod2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HelperMethod
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string myValue = superSecretFormula();
+			Console.WriteLine(myValue);
+			Console.ReadLine();
+		}
+
+		private string superSecretFormula()
+		{
+            //stuff here 
+            return "Hello World";
+
+		}
+
+        private static string superSecretFormula(string name)
+        {
+            return String.Format("Hello , {0}", name);
+
+
+        }
+
+
+
+	}
+}
+
+
+
+
+//private can only be called inside the class, unlike public
+//use camel case to indicate that it is private or scope
+//when you write a method, you are limiting what you need to change
+//can offload certain operations to other methods
+// name == input parameter
